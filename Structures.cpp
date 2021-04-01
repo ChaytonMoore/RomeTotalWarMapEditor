c9452a69befc1bf93ae328c4280e240d2b05f086
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-04-01 13:14:41 +0100

    Create Structures.cpp

---
+++ Structures.cpp
@@ -0,0 +1,15 @@
+#include "Structures.h"
+//This file contains all the functions regarding the the structures.
+
+
+std::vector<RowData> DeriveRoughness(std::vector<RowData> input)
+{
+  //This function generates the roughness from the heightmap.
+  //I'm going into this having no idea what's going to be the algorithm
+  
+ //Firstly might as well copy the  
+  
+  
+
+
+}
